1325152464d9c338b7993e68e87450b2e92987de
Rinnekioku <antoniifilonov@gmail.com>
2021-07-28 14:09:45 +0300

    modified styles and fixed bugs

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -1,139 +1,144 @@
-import React, {useCallback, useState, useEffect} from "react";
-import "firebase/auth";
-import "antd/dist/antd.css";
-import {Form, Upload, message} from "antd";
+import React, {useCallback, useState} from "react";
+import "./style.less";
+import {Form, Upload, message, Input, Button, PageHeader} from "antd";
+import {FormSC} from "./styled";
 import { UploadOutlined } from "@ant-design/icons";
 import ImgCrop from "antd-img-crop";
-import {FormSC, ItemSC, TextInputSC, PasswordInputSC, SubmitButtonSC, UploadButtonSC} from "./styled";
 import {auth, storage} from "../../services/firebase";
 
 export function Auth(props: any): JSX.Element {
     const [form] = Form.useForm();
-    const [fileList, setFileList] = useState<any>([]);
+    const [fileList, setFileList] = useState<any[]>([]);
 
     const registerUser = useCallback(async (event: any) => {
-        console.log("email", form.getFieldValue(["email"]), "password", form.getFieldValue(["password"]));
         event.preventDefault();
         try {
             await auth.createUserWithEmailAndPassword(form.getFieldValue(["email"]).trim(), form.getFieldValue(["password"]).trim());
             await auth.signInWithEmailAndPassword(form.getFieldValue(["email"]).trim(), form.getFieldValue(["password"]).trim());
-            await storage.ref().child(`images/${fileList[0].name}`).put(fileList[0]);
-        } catch(error) {
-            console.error(error);
+
+            const userId = auth.currentUser === null ? "anonymous" : auth.currentUser.uid;
+            const imgFile = storage.ref().child(`images/${userId}.png`);
+            const metadata = {
+                contentType: "image/jpeg",
+            };
+            const file = fileList[0];
+
+            await imgFile.put(file, metadata);
+            message.success("Signed up successfully");
+        } catch(e) {
+            message.error(e.message);
+            console.error(e);
         }
     }, [auth]);
 
-    useEffect(() => {
-        console.log(fileList);
-    }, [fileList]);
-
     const fakeLoad = useCallback(async({onError, onSuccess, file}) => {
         try {
-            onSuccess(null, file);
-            message.success("Picture loaded");
-        } catch (e){
-            message.error("Some error occured");
+            setFileList([file]);
+            message.success("Picture added successfully");
+        } catch(e) {
             onError(e);
+            message.error(e.message);
         }
-    },[]);
+    },[fileList]);
 
-    const addFiles = useCallback(({file}) => {
-        setFileList([file]);
-        console.log("file list");
-        console.log(fileList);
-    }, []);
+    const onChange = useCallback(({file}) => {
+        if (file.status === "removed"){
+            setFileList([]);
+        }
+    }, [fileList]);
 
     return (
-        <>
-            <FormSC
-                onSubmitCapture={registerUser}
-                form={form}
-                name="register"
+        <FormSC
+            onSubmitCapture={registerUser}
+            form={form}
+            name="register"
+        >
+            <PageHeader
+                title="Sign up"
+            />
+            <Form.Item
+                name="email"
+                label="E-mail"
+                rules={[
+                    {
+                        type: "email",
+                        message: "invalid email",
+                    },
+                    {
+                        required: true,
+                        message: "email field can't be empty",
+                    }
+                ]}
             >
-                <ItemSC
-                    name="email"
-                    label="E-mail"
-                    rules={[
-                        {
-                            type: "email",
-                            message: "invalid email",
-                        },
-                        {
-                            required: true,
-                            message: "email field can't be empty",
+                <Input/>
+            </Form.Item>
+            <Form.Item
+                name="password"
+                label="Password"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input your password!",
+                    },
+                    () => ({validator(_ ,value) {
+                        if (value === "" || value.length > 10){
+                            console.log(value);
+                            return Promise.resolve();
+                        } else {
+                            return Promise.reject(new Error("Too easy password"));
                         }
-                    ]}
-                >
-                    <TextInputSC/>
-                </ItemSC>
-                <ItemSC
-                    name="password"
-                    label="Password"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your password!",
-                        },
-                        () => ({validator(_ ,value) {
-                            if (value === "" || value.length > 10){
-                                console.log(value);
+                    },
+                    }),
+                ]}
+                hasFeedback
+            >
+                <Input.Password/>
+            </Form.Item>
+            <Form.Item
+                name="confirm"
+                label="Confirm Password"
+                dependencies={["password"]}
+                hasFeedback
+                rules={[
+                    {
+                        required: true,
+                        message: "Please confirm your password!",
+                    },
+                    ({ getFieldValue }) => ({
+                        validator(_, value) {
+                            if (!value || getFieldValue("password") === value) {
                                 return Promise.resolve();
-                            } else {
-                                return Promise.reject(new Error("Too easy password"));
                             }
+                            return Promise.reject(new Error("The two passwords that you entered do not match!"));
                         },
-                        }),
-                    ]}
-                    hasFeedback
-                >
-                    <PasswordInputSC/>
-                </ItemSC>
-                <ItemSC
-                    name="confirm"
-                    label="Confirm Password"
-                    dependencies={["password"]}
-                    hasFeedback
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please confirm your password!",
-                        },
-                        ({ getFieldValue }) => ({
-                            validator(_, value) {
-                                if (!value || getFieldValue("password") === value) {
-                                    return Promise.resolve();
-                                }
-                                return Promise.reject(new Error("The two passwords that you entered do not match!"));
-                            },
-                        }),
-                    ]}
-                >
-                    <PasswordInputSC />
-                </ItemSC>
-                <ItemSC
-                    name="avatar"
-                    label="Profile picture"
-                >
-                    <ImgCrop rotate>
-                        <Upload
-                            customRequest={fakeLoad}
-                            fileList={fileList}
-                            onChange={addFiles}
+                    }),
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+            <Form.Item
+                name="avatar"
+                label="Profile picture"
+            >
+                <ImgCrop rotate>
+                    <Upload
+                        customRequest={fakeLoad}
+                        fileList={fileList}
+                        onChange={onChange}
+                    >
+                        <Button
+                            icon={<UploadOutlined/>}
                         >
-                            <UploadButtonSC
-                                icon={<UploadOutlined/>}
-                            >
-                                Upload
-                            </UploadButtonSC>
-                        </Upload>
-                    </ImgCrop>
-                </ItemSC>
-                <ItemSC>
-                    <SubmitButtonSC htmlType="submit">
-                        Sign Up
-                    </SubmitButtonSC>
-                </ItemSC>
-            </FormSC>
-        </>
+                            Upload
+                        </Button>
+                    </Upload>
+                </ImgCrop>
+            </Form.Item>
+            <Form.Item>
+                <Button htmlType="submit">
+                    Sign Up
+                </Button>
+            </Form.Item>
+        </FormSC>
     );
 }
--- src/components/auth/styled.ts
+++ src/components/auth/styled.ts
@@ -1,90 +1,10 @@
 import styled from "styled-components";
-import {Form, Input, Button} from "antd";
+import {Form} from "antd";
 
 export const FormSC = styled(Form)`
-    .ant-input-password:focus{
-        border: none !important;
-        box-shadow: none !important;
-    }
-    .ant-input:focus {
-        box-shadow: 0 0 0 2px rgba(192, 47, 47, 0.5);
-    }
-    .ant-input-affix-wrapper-focused{
-        box-shadow: 0 0 0 2px rgba(192, 47, 47, 0.5) !important;
-    }
-    .ant-form-item-has-error :not(.ant-input-disabled).ant-input,
-    .ant-form-item-has-error :not(.ant-input-affix-wrapper-disabled).ant-input-affix-wrapper,
-    .ant-form-item-has-error :not(.ant-input-disabled).ant-input:hover,
-    .ant-form-item-has-error :not(.ant-input-affix-wrapper-disabled).ant-input-affix-wrapper:hover{
-        input {
-            box-shadow: none !important;
-        }
-    }
-    
-    .ant-form-item-has-error :not(.ant-input-disabled).ant-input:focus,
-    .ant-form-item-has-error :not(.ant-input-affix-wrapper-disabled).ant-input-affix-wrapper:focus,
-    .ant-form-item-has-error :not(.ant-input-disabled).ant-input-focused,
-    .ant-form-item-has-error :not(.ant-input-affix-wrapper-disabled).ant-input-affix-wrapper-focused{
-        box-shadow: 0 0 0 2px rgba(192, 47, 47, 0.5);
-    }
-    
     border: none;
     border-radius: 15px;
     box-shadow: 0 0 1rem 1rem rgba(0, 0, 0, 0.3);
     text-align: right;
     padding: 2rem 2rem 0.2rem;
     margin: 5rem 5rem;`;
-
-export const ItemSC = styled(Form.Item)`
-    `;
-
-export const TextInputSC = styled(Input)`
-    background: transparent !important;
-    .ant-input:focus {
-        box-shadow: none !important;
-    }
-    border: none;
-    border-radius: 10px;`;
-
-export const PasswordInputSC = styled(Input.Password)`
-    background: transparent !important;
-    border: none;
-    border-radius: 10px;`;
-
-export const SubmitButtonSC = styled(Button)`
-    background: #C02F2F;
-    border: none;
-    border-radius: 10px;
-    padding: 0.25rem;
-    color: #EDF6ED;
-    font-weight: bold;
-    padding-bottom: 1rem;
-    box-shadow: 0 3px #B02F2F;
-    transition: all 0.3s;
-    
-    :hover{
-        transform: translateY(1px);
-        box-shadow: 0 3px #A02F2F;
-    }
-    :active{
-        background: #B22B2B;
-        transform: translateY(1px);
-        box-shadow: 0 1px #A02F2F;
-        color: #EDF6ED;
-    }
-    &:hover, &:focus {
-        color: #EDF6ED !important;
-        background: #C02F2F !important;
-    }`;
-
-
-export const UploadButtonSC = styled(Button)`
-    background: transparent;
-    color: #c02f2f;
-    border-radius: 10px;
-    transition: all 0.5s ease-out;
-    &:hover, &:focus, &:active{
-        border-color: #d9d9d9;
-        color: #EDF6ED;
-        background: #c02f2f;
-    }`;
