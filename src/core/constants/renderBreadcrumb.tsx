617de37999487f05b3f99c4fb89e267e7f02e2ec
Rinnekioku <antoniifilonov@gmail.com>
2021-08-19 18:06:20 +0300

    removed unnessesary imports

--- src/core/constants/renderBreadcrumb.tsx
+++ src/core/constants/renderBreadcrumb.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, LinkProps } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Route } from 'antd/lib/breadcrumb/Breadcrumb';
 import i18n from 'i18next';
 
