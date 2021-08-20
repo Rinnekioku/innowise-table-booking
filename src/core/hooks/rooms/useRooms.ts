32d04a98f198fd868067a5a02a59285ea5f68dbb
Rinnekioku <antoniifilonov@gmail.com>
2021-08-20 14:35:13 +0300

    deploy commit

--- src/core/hooks/rooms/useRooms.ts
+++ src/core/hooks/rooms/useRooms.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { db } from '../../firebase';
